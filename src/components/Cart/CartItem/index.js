bf594c94a7ba1a73c4f9f1b4334c605f29dbf675
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-01-28 10:59:18 +0100

    table

---
+++ src/components/Cart/CartItem/index.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { removeCartItem, addProduct, reduceCartItem } from '../../../redux/Cart/cartActions'
+
+import { FaTrash, FaArrowAltCircleDown, FaArrowAltCircleUp } from 'react-icons/fa'
+
+
+const CartItem = ({item, pos}) => {
+    const dispatch = useDispatch();
+
+    const {
+        name,
+        images,
+        id,
+        description,
+        quantity,
+        price,
+    } = item;
+
+
+    const handleRemoveCartItem = (itemID) => {
+        dispatch(
+            removeCartItem({
+                itemID
+        })
+        )
+    }
+
+    const handleAddProduct = (product) => {
+        dispatch(
+            addProduct(product)
+        )
+    }
+
+    const handleReduceProduct = (product) => {
+        dispatch(
+            reduceCartItem(product)
+        )
+    }
+
+
+    return (
+        <table className="cart-item" border="0" cellSpacing="0" cellPadding="10">
+            <tbody>
+              <tr key={pos}>
+                <td>
+                    <img src={images} alt={name}/>
+                </td>
+                <td className="del-desc">
+                    {description}
+                </td>
+                <td className="quantity-group">
+                    <span className="btn arrow" onClick={() => handleReduceProduct(item)}> 
+                        <FaArrowAltCircleDown />
+                    </span>
+                    <span>{quantity}</span>
+                    <span className="btn arrow" onClick={() => handleAddProduct(item)}> 
+                        <FaArrowAltCircleUp />
+                    </span>
+                </td>
+                <td>
+                    {price}
+                </td>
+                <td>
+                    <button className="btn delete" onClick={() => handleRemoveCartItem(id) }>
+                        <FaTrash />
+                    </button>
+                </td>
+              </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default CartItem
---
+++ src/components/Cart/index.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { selectCartItems, selectCartTotal } from '../../redux/Cart/cartSelectors'
+import { createStructuredSelector } from 'reselect'
+
+
+import './styles.css';
+import { Link, useHistory } from 'react-router-dom';
+
+import { Button, BtnLink } from '../../globalStyles'
+
+import Item from '../Checkout/Item/'
+
+const mapState = createStructuredSelector({
+    cartItems: selectCartItems,
+    total: selectCartTotal
+})
+
+
+const Checkout = () => {
+    const { cartItems, total } = useSelector(mapState)
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const errorMsg = 'No Items  to Display';
+
+
+    return (
+        <div className="checkout">
+            <h1>Checkout</h1>
+
+            <div className="cart">
+                { cartItems.length > 0 ? (
+                <table border="0" cellPadding="0" cellSpacing="0">
+                    <tbody>
+                        <tr>
+                           <table className="checkout-header" border="0" cellPadding="0" cellSpacing="0">
+                            <tbody>
+                                <tr>
+                                    <th>Product</th>
+                                    <th className="del-desc">Description</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    <th className="del-trash">Remove</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </tr>
+                        <tr>
+                            <table className="main-table" border="0" cellPadding="0" cellSpacing="0">
+                                <tbody>
+                                    {cartItems.map((item, pos) => {
+                                        return (
+                                            <Item item={item} pos={pos} />
+                                        )
+                                      })
+                                    }
+                                </tbody>
+                            </table>
+                        </tr>
+                        <tr>
+                            <table className="total-cart" border="0" cellPadding="0" cellSpacing="0" >
+                                <tr >
+                                    <td>
+                                        <h3>
+                                            Total: {`\u20A6`}{total}
+                                        </h3>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <table className="btn_group" border="0" cellPadding="0" cellSpacing="0">
+                                        <tbody>
+                                            <tr>
+                                                <td className="cart-btn">
+                                                    <Button pay fontSmall full>
+                                                       <BtnLink onClick={() => history.push('/product')}> 
+                                                          Continue Shopping 
+                                                       </BtnLink>
+                                                    </Button>
+                                                    
+                                                </td>
+                                                <td className="cart-btn">
+                                                    <Button pay fontSmall full>
+                                                       <BtnLink to="/payment">
+                                                         Check Out
+                                                       </BtnLink>
+                                                    </Button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </tr>
+                            </table>
+                        </tr>
+                    </tbody>
+                </table>
+                ) : (
+                    <p className="errorMsg">
+                        {errorMsg}
+                    </p>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Checkout;
