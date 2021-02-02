10c0ea27259dadf56f66ab5cad3604608c882d92
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2021-02-02 15:04:18 +0100

    color added to like image

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -95,8 +95,8 @@ const ProductDetails = (props) => {
                     type="button"
                      onClick={() => handleAddToCart(productSingle)}>Add to Cart</button>
         </div>
-        <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300" type="button" aria-label="like">
-            <svg width="20" height="20" fill="currentColor">
+        <button class="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300 focus:text-red-400" type="button" aria-label="like">
+            <svg width="20" height="20" fill="currentColor" onFocus="">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
             </svg>
         </button>
